da682fd911f5924022a1f578c4b1aa2c76715ec8
Trey Smith <slicksmith14@gmail.com>
2024-08-09 14:55:06 -0400

    Add 2 new modules, projects and tasks

---
+++ src/projects.js
@@ -0,0 +1,20 @@
+class Projects {
+    constructor(name) {
+        this.name = name;
+        this.tasks = [];
+    }
+
+    setName(name) {
+        this.name = name;
+    }
+    getName() {
+        return this.name;
+    }
+
+    setTasks(tasks) {
+        this.tasks = tasks;
+    }
+    getTasks() {
+        return this.tasks;
+    }
+}
---
+++ src/tasks.js
@@ -0,0 +1,52 @@
+class Tasks {
+    constructor(name, description, priority, notes, dueDate, createtionDate) {
+        this.name = name;
+        this.description = description;
+        this.priority = priority;
+        this.notes = notes;
+        this.dueDate = dueDate;
+        this.createtionDate = createtionDate;
+    }
+
+    setName(name) {
+        this.name = name;
+    }
+    getName() {
+        return this.name;
+    }
+
+    setDescription(description) {
+        this.description = description;
+    }
+    getDescription() {
+        return this.description;
+    }
+
+    setPriority(priority) {
+        this.priority = priority;
+    }
+    getPriority() {
+        return this.priority;
+    }
+
+    setNotes(notes) {
+        this.notes = notes;
+    }
+    getNotes() {
+        return this.notes;
+    }
+
+    setDueDate(dueDate) {
+        this.dueDate = dueDate;
+    }
+    getDueDate() {
+        return this.dueDate;
+    }
+
+    setCreationDate(createtionDate) {
+        this.createtionDate = createtionDate;
+    }
+    getCreationDate() {
+        return this.createtionDate;
+    }
+}
